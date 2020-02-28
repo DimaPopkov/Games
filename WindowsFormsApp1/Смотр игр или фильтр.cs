6b78bf59498f52c4ed03f69af5aed9651d144ca6
unknown <dimagame73ulru@example.com>
2020-02-28 15:40:59 +0400

    Удалил лишние файлы

---
+++ WindowsFormsApp1/Смотр игр или фильтр.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+
+
+
+        private void buttonTanks_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            GameInfoForm form = new GameInfoForm(btn.Tag.ToString(), btn.AccessibleName, Convert.ToInt32(btn.AccessibleDescription));
+            form.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();
+            form.Show();
+
+        }
+    }
+}
